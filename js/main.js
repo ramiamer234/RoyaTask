e2e420b7894d8aece9bbfd476ed0331dc5e6cd8d
Shadi Shomali <32237641+shadishomali@users.noreply.github.com>
2019-11-09 15:34:51 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function showMore() {
+    var x = document.getElementById("moreItems");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
